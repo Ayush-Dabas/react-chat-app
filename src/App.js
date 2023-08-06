68e3b5fb73b732d4a37a1aef108e11c2997595c8
Ayush-Dabas <aydbs23@gmail.com>
2023-08-06 05:39:46 +0530

    this is getting out of hand

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Navbar, SignUp } from "./components/index";
+// import { Navbar, SignUp } from "./components/index";
 
 function App() {
   return (
     <div className="App">
-      <SignUp />
+      {/* <SignUp /> */}
       {/* <Navbar /> */}
     </div>
   );
